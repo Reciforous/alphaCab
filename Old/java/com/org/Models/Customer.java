0250459aa4cd6de12e56566cb4728eb4ca6997c9
Mohamed Zain Riyaz <mohamedzainriyaz@gmail.com>
2019-10-14 17:33:32 +0500

    - Finished up add and update functions for Customer class need to test

--- Old/java/com/org/Models/Customer.java
+++ Old/java/com/org/Models/Customer.java
@@ -4,13 +4,14 @@ import com.org.Helpers.Message;
 import com.org.Helpers.Functions;
 import com.org.Models.Db;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Customer {
     public Integer id = null;
     public String name = null;
-    public String Address = null;
+    public String address = null;
 
     public Message createTable(){
         Message message = new Message();
@@ -48,6 +49,84 @@ public class Customer {
                 Functions.printSQLError(e);
             }
         }
+        return message;
+    }
+
+    public Message add(){
+        Message message = new Message();
+        String sql = "insert into Customer (id, name, address) values (?, ?, ?)";
+
+        Db db = new Db();
+        db.getConnection();
+
+        try(PreparedStatement pstmt = db.connection.prepareStatement(sql)){
+            pstmt.setInt(1, this.id);
+            pstmt.setString(2, this.name);
+            pstmt.setString(3, this.address);
+            pstmt.executeUpdate();
+
+            message = new Message(
+                    true,
+                    "Customer added successfully",
+                    "success"
+            );
+        }
+        catch (SQLException e){
+            message = new Message(
+                    false,
+                    "Error: " + e.getMessage(),
+                    "error"
+            );
+            Functions.printSQLError(e);
+        }
+        finally {
+            try{
+                db.connection.close();
+            }
+            catch (SQLException e){
+                Functions.printSQLError(e);
+            }
+        }
+
+        return message;
+    }
+
+    public Message update(){
+        Message message = new Message();
+        String sql = "update Customer SET name = ?, address = ? where id = ?";
+
+        Db db = new Db();
+        db.getConnection();
+
+        try(PreparedStatement pstmt = db.connection.prepareStatement(sql)){
+            pstmt.setString(1, this.name);
+            pstmt.setString(2, this.address);
+            pstmt.setInt(3, this.id);
+            pstmt.executeUpdate();
+
+            message = new Message(
+                    true,
+                    "Customer " + this.id +  " details updated successfully",
+                    "success"
+            );
+        }
+        catch (SQLException e){
+            message = new Message(
+                    false,
+                    "Error: " + e.getMessage(),
+                    "error"
+            );
+            Functions.printSQLError(e);
+        }
+        finally {
+            try{
+                db.connection.close();
+            }
+            catch (SQLException e){
+                Functions.printSQLError(e);
+            }
+        }
+
         return message;
     }
 }
