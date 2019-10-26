2714fad0839b3b5ccab9d65f8aa3b724113873fb
shaaik.naseer <shaaik.naseer@mma.gov.mv>
2019-10-26 18:29:16 +0500

    Created Drivers View

---
+++ alphaCab/src/java/com/org/Controllers/ViewDrivers.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.org.Controllers;
+
+import com.org.Models.Driver;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author shaaik
+ */
+public class ViewDrivers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            ArrayList<Driver> drivers = Driver.getDrivers();
+            System.out.println("test");
+
+            System.out.println(drivers.size());
+            System.out.println(drivers.get(1).name);
+            
+            request.setAttribute("drivers", drivers);
+
+            request.getRequestDispatcher("/views/admin/drivers.jsp").forward(request, response);
+        }
+        catch (ServletException e){
+            response.getWriter().print("There was an error handling your request, Please go back!\n" + e.getMessage());
+        }
+    }
+}
---
+++ alphaCab/web/views/admin/drivers.jsp
@@ -0,0 +1,40 @@
+<%-- 
+    Document   : drivers
+    Created on : Oct 26, 2019, 5:42:10 PM
+    Author     : shaaik
+--%>
+
+<%@page import="com.org.Models.Driver"%>
+<%@page import="java.util.ArrayList"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Drivers!</h1>
+        <% ArrayList<Driver> drivers = (ArrayList<Driver>) request.getAttribute("drivers"); %>
+        
+    <table style="border: 1px solid black;">
+        <tr>
+            <th style="border: 1px solid black;">Name</th>
+            <th style="border: 1px solid black;">Registration</th>
+        </tr>
+        <% for(Driver driver : drivers){%>
+            <tr style="border: 1px solid black;">
+                <td style="border: 1px solid black;">
+                    <% out.print(driver.name); %>
+                </td> 
+                <td style="border: 1px solid black;">                
+                    <% out.print(driver.registration); %>
+                </td>        
+            </tr>
+        <%}%>
+    </table>
+
+    </body>
+</html>
