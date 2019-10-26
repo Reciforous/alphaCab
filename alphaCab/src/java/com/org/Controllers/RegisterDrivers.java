97d817fcbd4de7a3304e54a1eed579e40f98bc8d
shaaik.naseer <shaaik.naseer@mma.gov.mv>
2019-10-26 17:23:07 +0500

    Added Driver registartion

---
+++ alphaCab/src/java/com/org/Controllers/RegisterDrivers.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.org.Controllers;
+
+import com.org.Helpers.Message;
+import com.org.Models.Demand;
+import com.org.Models.Driver;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author shaaik
+ */
+public class RegisterDrivers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            request.getRequestDispatcher("/views/driver/register.jsp").forward(request, response);
+        }
+        catch (ServletException e){
+            response.getWriter().print("There was an error handling your request, Please go back!\n" + e.getMessage());
+        }
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        String name = request.getParameter("name");
+        String registration = request.getParameter("registration");
+        
+        System.out.println(name);
+        System.out.println(registration);
+        
+        Driver driver = new Driver(name, registration);           
+        Message message = driver.add();
+        System.out.println(message.content);
+        response.sendRedirect("/alphaCab/admin/home");
+ 
+    }
+}
