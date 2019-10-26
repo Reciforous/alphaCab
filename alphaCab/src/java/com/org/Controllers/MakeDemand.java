6787065d6fc6f0bad92f6d1eb9fe80e23a8cb7cd
Mohamed Zain Riyaz <mohamedzainriyaz@gmail.com>
2019-10-27 01:16:10 +0500

    - Fixed some bugs in MakeDemand controller, properly gets customer ID from cookie

--- alphaCab/src/java/com/org/Controllers/MakeDemand.java
+++ alphaCab/src/java/com/org/Controllers/MakeDemand.java
@@ -1,9 +1,11 @@
 package com.org.Controllers;
 
 import com.org.Helpers.Message;
+import com.org.Models.Customer;
 import com.org.Models.Demand;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +14,7 @@ import java.io.IOException;
 public class MakeDemand extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try{
+            request.setAttribute("message", null);
             request.getRequestDispatcher("/alphacab/views/customer/make-demand.jsp").forward(request, response);
         }
         catch (ServletException e){
@@ -21,11 +24,27 @@ public class MakeDemand extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
         try{
-            String name = (String) request.getParameter("name");
+            Cookie[] cookies = request.getCookies();
+            Cookie id_cookie = null;
+            Integer id = null;
+
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("id")){
+                    id_cookie = cookie;
+                }
+            }
+
+            if(id_cookie != null){
+                id = Integer.parseInt(id_cookie.getValue());
+            }
+
+            Customer customer = new Customer(id);
+            customer.get();
+
             String address = (String) request.getParameter("address");
             String destination = (String) request.getParameter("destination");
 
-            Demand demand = new Demand(name, address, destination);
+            Demand demand = new Demand(customer.name, address, destination);
             Message message = demand.add();
 
             request.setAttribute("message", message);
