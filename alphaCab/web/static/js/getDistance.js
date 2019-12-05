340eedc10bd51d67585d8ed0025c5025c025d7b0
Mohamed Zain Riyaz <mohamedzainriyaz@gmail.com>
2019-12-06 02:42:34 +0500

    - Address and Destination Inputs are now stored as latitude and longitude from frontend

--- alphaCab/web/static/js/getDistance.js
+++ alphaCab/web/static/js/getDistance.js
@@ -5,18 +5,26 @@ var app = new Vue({
 		destination: $('#destination').val(),
 		distance: null,
 		distanceText: null,
-		amount: null
+		amount: null,
+		addressText: null,
+		destinationText: null
 	},
 	methods: {
 		getDistance(){
 			console.log(this.address)
 			console.log(this.destination)
 
+			this.address = this.address.split(", ")
+			origin = new google.maps.LatLng(parseFloat(this.address[0]), parseFloat(this.address[1]))
+
+			this.destination = this.destination.split(", ")
+			destination = new google.maps.LatLng(parseFloat(this.destination[0]), parseFloat(this.destination[1]))
+
 			service = new google.maps.DistanceMatrixService()
 			service.getDistanceMatrix(
 				{
-					origins: [this.address],
-					destinations: [this.destination],
+					origins: [origin],
+					destinations: [destination],
 					travelMode: 'DRIVING'
 				},
 
@@ -28,6 +36,10 @@ var app = new Vue({
 		  if (status == 'OK') {
 		    var origins = response.originAddresses
 		    var destinations = response.destinationAddresses
+		    this.addressText = response.originAddresses[0]
+		    this.destinationText = response.destinationAddresses[0]
+
+		   	console.log(response)
 
 		    for (var i = 0; i < origins.length; i++) {
 		      var results = response.rows[i].elements
@@ -41,7 +53,7 @@ var app = new Vue({
 		  }
 		}
 	},
-	mounted(){
+	mounted() {
 		this.getDistance()
 	}
 })
